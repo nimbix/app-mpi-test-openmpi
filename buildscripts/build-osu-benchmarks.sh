7f9e4727ae2d59f3117b27b4b2dadc687967d0e3
Johnathan Hegge <johnathan.hegge@nimbix.net>
2021-08-25 15:52:36 -0500

    JAR-4433
    prepare the install directory

--- buildscripts/build-osu-benchmarks.sh
+++ buildscripts/build-osu-benchmarks.sh
@@ -3,6 +3,11 @@
 # Find the Open MPI root we'll be using
 [[ -r /usr/local/bin/setup_openmpi.sh ]] && source /usr/local/bin/setup_openmpi.sh
 
+# Prepare the install dir
+BENCH_DIR=/usr/local/libexec/osu-micro-benchmarks/mpi
+sudo mkdir -p $BENCH_DIR
+sudo chmod -R a+w $BENCH_DIR
+
 # source added by mpi-common, installed to /usr/local/libexec
 cd $MPI_COMMON/osu-benchmarks || exit 1
 
